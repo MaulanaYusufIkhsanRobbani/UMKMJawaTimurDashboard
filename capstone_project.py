5dd7e624e222e0d28e7cc083ea4249d530dd0768
MaulanaYusufIkhsanRobbani <158140236+MaulanaYusufIkhsanRobbani@users.noreply.github.com>
2024-02-29 11:46:14 +0700

    Update capstone_project.py

--- capstone_project.py
+++ capstone_project.py
@@ -28,7 +28,7 @@ st.markdown(
 
 st.header('Disclaimer 	:heavy_exclamation_mark:')
 st.text('Data diambil berdasarkkan jawaban responden dari pelaku/karyawan UMKM yang memungkinkan diisi tidak sesuai realita')
-tabel = pd.read_csv('../projectDQLab/data_umkm_ke4_openrevine_202402171115.csv')
+tabel = pd.read_csv('https://raw.githubusercontent.com/MaulanaYusufIkhsanRobbani/UMKMJawaTimurDashboard/main/data_umkm_ke4_openrevine_202402171115.csv')
 numerik = ['ID_DT_BINAAN_EXCEL','TAHUN_MULAI_USAHA','UMUR',
            'JML_TENAGA_KERJA_2019','JML_TENAGA_KERJA_2020',
            'JML_TENAGA_KERJA_2021','OMSET_2019','OMSET_2020',
